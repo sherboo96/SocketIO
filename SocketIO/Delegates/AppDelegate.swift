53187d135245a03ceaa30aca9e882e68123830f0
sherbeny96 <sherbeny2096@gmail.com>
2021-10-04 17:22:21 +0200

    Convert to Xib Style

---
+++ SocketIO/Delegates/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SocketIO
+//
+//  Created by Mahmoud Sherbeny on 04/10/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+    
+}
+

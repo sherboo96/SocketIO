272273ac05840b5b8788aaf7dc397d5a0c081ada
sherbeny96 <sherbeny2096@gmail.com>
2021-10-04 17:16:20 +0200

    Initial Commit

---
+++ SocketIO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketIO
+//
+//  Created by Mahmoud Sherbeny on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
